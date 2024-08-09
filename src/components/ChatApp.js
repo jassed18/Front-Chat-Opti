122350b8d10ee0cd442d5f8bc161f189d33ec546
Adrián Pabón Mendoza <afpabon@uninorte.edu.co>
2024-08-09 16:51:57 -0500

    sessions

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -18,9 +18,17 @@ const ChatApp = () => {
 
   const handleSendMessage = async (message) => {
     setMessages([...messages, { type: 'text', content: message, sender: 'user' }]);
+    let sessionId = localStorage.getItem('session_id');
+
     try {
-      const response = await axios.post(process.env.REACT_APP_API_URL, { message });
-      const botResponses = response.data.output.generic;
+      const response = await axios.post(process.env.REACT_APP_API_URL, { message, session_id: sessionId });
+      const data = response.data;
+
+      if (data.session_id) {
+        localStorage.setItem('session_id', data.session_id);
+      }
+
+      const botResponses = data.response.output.generic;
 
       botResponses.forEach((botResponse) => {
         if (botResponse.response_type === 'text') {
@@ -40,8 +48,44 @@ const ChatApp = () => {
     }
   };
 
-  const handleSendAudio = (audioBlob) => {
+  const handleSendAudio = async (audioBlob) => {
     setMessages([...messages, { type: 'audio', content: URL.createObjectURL(audioBlob), sender: 'user' }]);
+    let sessionId = localStorage.getItem('session_id');
+
+    const formData = new FormData();
+    formData.append('file', audioBlob);
+    formData.append('session_id', sessionId);
+
+    try {
+      const response = await axios.post(process.env.REACT_APP_API_URL, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      const data = response.data;
+
+      if (data.session_id) {
+        localStorage.setItem('session_id', data.session_id);
+      }
+
+      const botResponses = data.response.output.generic;
+
+      botResponses.forEach((botResponse) => {
+        if (botResponse.response_type === 'text') {
+          setMessages((prevMessages) => [
+            ...prevMessages,
+            { type: 'text', content: botResponse.text, sender: 'bot' },
+          ]);
+        } else if (botResponse.response_type === 'option') {
+          setMessages((prevMessages) => [
+            ...prevMessages,
+            { type: 'option', options: botResponse.options, sender: 'bot' },
+          ]);
+        }
+      });
+    } catch (error) {
+      console.error('Error sending audio:', error);
+    }
   };
 
   const handleOptionClick = (optionText) => {
